ce80aa17c654569f0fa0c1da80b5f2e2a4d7de2a
belllead <sociojs85@gmail.com>
2022-02-06 15:20:12 +0900

    프로젝트 생성

---
+++ main.js
@@ -0,0 +1,17 @@
+import _ from 'lodash' // From 'node_modules'
+
+console.log('hello world')
+console.log(_.camelCase('hello world'))
+
+// import getType from './getType' // getType.js
+// import getRandom from './getRandom' // getRandom.js
+
+
+// console.log(checkType([1, 2, 3]))
+// console.log(getRandom(), getRandom())
+
+// const numbers = [1, 2, 3, 4]
+
+// numbers.splice(2, 3)
+// console.log(numbers)
+
